6e0723274e5a240b89ecb70f4b1b005e6adafc66
Linjiaqin <JiaqinLin@outlook.com>
2019-05-21 22:32:57 +0800

    add comment

--- testDownLoadDialog.py
+++ testDownLoadDialog.py
@@ -5,13 +5,13 @@ class downloadDialog(QDialog,Ui_Dialog):
     def __init__(self):
         super(downloadDialog,self).__init__()
         self.setupUi(self)
+        #进度条数值设为0
         self.Progress.setValue(0)
-       # self.TotalSize = 0
         
         
     def slotFun(self,value):
+        # 更新进度条数值
         self.Progress.setValue(value)
-        #self.Progress.setRange(value/100*self.TotalSize,self.TotalSize)
 
     def merge(self):
         self.lbDownLoading.setText("下载完成")
@@ -21,6 +21,7 @@ class downloadDialog(QDialog,Ui_Dialog):
         self.lbCur.setText("格式转换成功，关闭窗口然后去看看吧")
 
     def setTotalSize(self,value):
+        # 设置大小标签
         self.TotalSize = value/1024/1024
         self.Progress.setRange(0,self.TotalSize)
 
